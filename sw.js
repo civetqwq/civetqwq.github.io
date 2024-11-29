5caea473dc33c9eeff0ec617dd75884cd27199ec
civetqwq <2777981992@qq.com>
2024-11-29 16:15:18 +0800

    Site updated: 2024-11-29 16:15:08

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732800355168";
+const VERSION = "1732868092824";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
