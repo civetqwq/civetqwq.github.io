9bb61663daca81d734131652ac2bede0d253e25c
civetqwq <2777981992@qq.com>
2024-12-18 09:05:54 +0800

    Site updated: 2024-12-18 09:05:54

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1734481696631";
+const VERSION = "1734483946662";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
