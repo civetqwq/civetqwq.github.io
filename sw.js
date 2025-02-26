30d4393d226774f5321bc77e6cc9eef08c559315
civetqwq <2777981992@qq.com>
2025-02-26 15:59:43 +0800

    Site updated: 2025-02-26 15:59:43

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1740474903019";
+const VERSION = "1740556748027";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
