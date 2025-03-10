dfeccb0a400189e9fcfb0e682922e83aa0471114
civetqwq <2777981992@qq.com>
2025-03-10 21:37:58 +0800

    Site updated: 2025-03-10 21:37:56

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1741612831270";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1741613864973";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
